303a8306484c85767f1917271d280bc6c7a2c5c5
Mattheo <mattheo.guzman1@gmail.com>
2025-06-10 11:20:45 +0000

    planning module v1.8

--- planning_multi_employee/__manifest__.py
+++ planning_multi_employee/__manifest__.py
@@ -2,18 +2,15 @@
     'name': 'Planning Multi Employee',
     'version': '1.0',
     'summary': 'Permet d\'assigner plusieurs employés à un créneau de planning',
-    'description': "Ce module étend le module Planning d'Odoo pour permettre la sélection de plusieurs employées.",
+    'description': "Ce module étend le module Planning d'Odoo pour permettre la sélection de plusieurs employés.",
     'category': 'Human Resources',
     'author': 'Cortys SRL',
     'depends': ['planning', 'hr'],
     'data': [
-    'views/planning_slot_form_views.xml',
-    'views/planning_slot_list_views.xml',
-    'views/planning_slot_kanban_views.xml',
-    'views/planning_slot_template',
-],
-
+        'views/planning_slot_search_views.xml',
+        'views/planning_slot_views.xml',
+    ],
     'installable': True,
-    'application': True,
-    
+    'application': False,
+    'auto_install': False,
 }
--- planning_multi_employee/models/planning_slot.py
+++ planning_multi_employee/models/planning_slot.py
@@ -2,5 +2,20 @@ from odoo import fields, models
 
 class PlanningSlot(models.Model):
     _inherit = 'planning.slot'
+    
+    multi_employee_ids = fields.Many2many(
+        'hr.employee', 
+        string="Employés",
+        help="Sélectionnez plusieurs employés pour ce créneau"
+    )
+     # Solution définitive pour supprimer le champ
+    employee_skill_ids = None  # Suppression radicale au niveau Python
 
-    multi_employee_ids = fields.Many2many('hr.employee', string="Employees")
+class PlanningSlotTemplate(models.Model):
+    _inherit = 'planning.slot.template'
+    
+    multi_employee_ids = fields.Many2many(
+        'hr.employee', 
+        string="Employés",
+        help="Sélectionnez plusieurs employés pour ce modèle"
+    )
